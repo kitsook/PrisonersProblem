d5439020e8092d3629262b2738511ec127acfed6
Clarence Ho <clarenceho@gmail.com>
2022-07-02 02:49:14 -0700

    User specific thread pool

--- src/main/java/net/clarenceho/prisoners/App.java
+++ src/main/java/net/clarenceho/prisoners/App.java
@@ -6,12 +6,16 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ForkJoinPool;
 import java.util.stream.IntStream;
 
 @SpringBootApplication
 public class App implements CommandLineRunner{
   private static final Logger LOG = LoggerFactory
       .getLogger(App.class);
+
+  private static final int NUM_PARALLEL_THREAD = 8;
 
   private static final int NUM_PRISONER = 100;
   private static final int TRIAL_COUNT = 1_000_000;
@@ -21,15 +25,23 @@ public class App implements CommandLineRunner{
   }
 
   @Override
-  public void run(String... arg0) {
-    int winCount = IntStream.rangeClosed(1, TRIAL_COUNT)
-        .parallel()
-        .map(i -> {
-          PrisonerProblem problem = new PrisonerProblem(NUM_PRISONER);
-          return problem.prisonersWin()? 1 : 0;
-        })
-        .reduce(0, Integer::sum);
+  public void run(String... arg0) throws ExecutionException, InterruptedException {
+    ForkJoinPool customThreadPool = new ForkJoinPool(NUM_PARALLEL_THREAD);
+    try {
+      int winCount = customThreadPool.submit(() ->
+          IntStream.rangeClosed(1, TRIAL_COUNT)
+              .parallel()
+              .map(i -> {
+                PrisonerProblem problem = new PrisonerProblem(NUM_PRISONER);
+                return problem.prisonersWin()? 1 : 0;
+              })
+              .reduce(0, Integer::sum)
+      ).get();
 
-    LOG.info("Winning percentage = " + ((double)winCount / TRIAL_COUNT));
+      LOG.info("Winning percentage = " + ((double)winCount / TRIAL_COUNT));
+
+    } finally {
+      customThreadPool.shutdown();
+    }
   }
 }
